909b21381f28f5b20d76e9afc042bd2e531d4385
James Loh <jloh@squiz.net>
2016-04-26 14:20:19 +1000

    Fixing fish_greeting so it doesn't send a newline and adding a newline at the top of fish_prompt so everything is spaced out a bit nicer

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -87,6 +87,7 @@ function fish_prompt --description 'Write out the prompt'
 		)
 	end
 
+	echo ''
 	echo -s "[" (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal "$mode_str" "]"
 	echo -n -s "["(set_color cyan)(date "+%H")(set_color normal)':'(set_color cyan)(date "+%M")(set_color normal)':'(set_color cyan)(date "+%S")(set_color normal)']' $prompt_status ' $ '
 end
