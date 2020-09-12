b20544ac430fe1c642148e1d9d45e1cc3c91c65d
James Loh <git@jloh.co>
2020-09-12 18:30:25 +1000

    Add in linuxbrew config to fish

---
+++ init/50_linux.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+# Add more stuff to our path
+# Linux homebrew is in a different dir so add it to our paths
+set -U fish_user_paths /home/linuxbrew/.linuxbrew/bin $fish_user_paths
